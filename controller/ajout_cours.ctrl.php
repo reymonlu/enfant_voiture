7b54f403f3beb39908139467da8bae5831f848f8
anti-moi <valentindan@hotmail.com>
2018-10-03 16:42:55 +0200

    On retravaille un peu les controllers

--- controller/ajout_cours.ctrl.php
+++ controller/ajout_cours.ctrl.php
@@ -1,10 +1,16 @@
 <?php
   $_GET['libelle'] = htmlentities($_GET['libelle']);
-  $_GET['heure_debut'] = htmlentities($_GET['heure_debut']);
+  $_GET['heure_deb'] = htmlentities($_GET['heure_deb']);
   $_GET['duree'] = htmlentities($_GET['duree']);
   $_GET['jour_sem'] = htmlentities($_GET['jour_sem']);
+
   $libelle = $_GET['libelle'];
-  $heure_debut = $_GET['heure_debut'];
-  $heure_fin = $_GET['heure_fin'];
+  $heure_deb= $_GET['heure_deb'];
+  $duree = $_GET['duree'];
   $jour_sem = $_GET['jour_sem'];
+
+  var_dump($libelle);
+  var_dump($heure_deb);
+  var_dump($duree);
+  var_dump($jour_sem);
 ?>
--- index.php
+++ index.php
@@ -7,32 +7,42 @@
   <body>
     <h1>Que voulez-vous faire?</h1>
     <h2>Rajouter un enfant</h2>
-      <form action="model/enfant.dao.php" method="get">
+      <form action="controller/ajout_enfant.ctrl.php" method="get">
         Prénom de l'enfant : <input type="text" name="prenom" /><br>
         Nom de l'enfant : <input type="text" name="nom" value=""><br>
         <input type="submit" name="valider" value="OK">
       </form>
     <h2>Rajouter un cours</h2>
-    <form action="model/cours.dao.php" method="get">
+    <form action="controller/ajout_cours.ctrl.php" method="get">
       Libellé : <input type="text" name="libelle" /><br>
-      Heure de début : <input type="text" name="heure_deb"><br>
-      Heure de fin : <input type="text" name="heure_fin"><br>
-      Jour de la semaine : <input type="text" name="jour_sem"><br>
+      Heure de début : <input type="time" name="heure_deb"><br>
+      Durée : <input type="number" name="duree"><br>
+      Jour de la semaine :
+        <select name="jour_sem">
+          <option value="lundi">Lundi</option>
+          <option value="mardi">Mardi</option>
+          <option value="mercredi">Mercredi</option>
+          <option value="jeudi">Jeudi</option>
+          <option value="vendredi">Vendredi</option>
+          <option value="samedi">Samedi</option>
+          <option value="dimanche">Dimanche</option>
+
+        </select>
       <input type="submit" name="valider" value="OK">
     </form>
     <h2>Rajouter un Tuteur</h2>
-    <form action="model/tuteur.dao.php" method="get">
+    <form action="controller/ajout_tuteur.ctrl.php" method="get">
       Prénom du tuteur : <input type="text" name="prenom" /><br>
       Nom du tuteur : <input type="text" name="nom"><br>
       Pourcentage de disponibilité : <input type="number" name="ptage_dispo" value="">
       Jours de disponibilité :<br>
-      <input type="checkbox" name="jour_dispo" value="1"> Lundi<br>
-      <input type="checkbox" name="jour_dispo" value="2"> Mardi<br>
-      <input type="checkbox" name="jour_dispo" value="3"> Mercredi<br>
-      <input type="checkbox" name="jour_dispo" value="4"> Jeudi<br>
-      <input type="checkbox" name="jour_dispo" value="5"> Vendredi<br>
-      <input type="checkbox" name="jour_dispo" value="6"> Samedi<br>
-      <input type="checkbox" name="jour_dispo" value="7"> Dimanche<br>
+      <input type="checkbox" name="jour_dispo[]" value="lundi"> Lundi<br>
+      <input type="checkbox" name="jour_dispo[]" value="mardi"> Mardi<br>
+      <input type="checkbox" name="jour_dispo[]" value="mercredi"> Mercredi<br>
+      <input type="checkbox" name="jour_dispo[]" value="jeudi"> Jeudi<br>
+      <input type="checkbox" name="jour_dispo[]" value="vendredi"> Vendredi<br>
+      <input type="checkbox" name="jour_dispo[]" value="samedi"> Samedi<br>
+      <input type="checkbox" name="jour_dispo[]" value="dimanche"> Dimanche<br>
       <input type="submit" name="valider" value="OK">
     </form>
 

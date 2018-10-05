a3f4d5783a5e7a7ff7768566be39d2229164547c
Loic BONNARD <bonnard.loic.07@gmail.com>
2018-10-05 14:20:13 +0200

    Last modif display done, creating add

--- vue/display.php
+++ vue/display.php
@@ -7,25 +7,24 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   <title>Bienvenue sur le site de Co-voiturage !</title>
+  <style>
+    div{
+      margin: auto;
+      width: 200px;
+    }
+    .tableau{
+      margin: auto;
+    }
+    td{
+      padding: 10px;
+      text-align: center;
+      border: 2px solid black;
+    }
+  </style>
 </head>
 
 <body>
 
-<!-- <form>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-danger">Submit</button>
-  </form> -->
   <form method="post" action="#">
     <div>
       <label>Table :</label>
@@ -35,31 +34,34 @@
       <button type="submit" class="btn btn-danger" name="SubmitButton">Submit</button>
     </div>
   </form>
+
+
   <?php
- echo "<table style='border: solid 1px black;'>";
- echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
-
- class TableRows extends RecursiveIteratorIterator
+ function display_data($data)
  {
-     public function __construct($it)
-     {
-         parent::__construct($it, self::LEAVES_ONLY);
+     $output = "<table class='tableau'>";
+     foreach ($data as $key => $var) {
+         //$output .= '<tr>';
+         if ($key===0) {
+             $output .= '<tr>';
+             foreach ($var as $col => $val) {
+                 $output .= "<td>" . $col . '</td>';
+             }
+             $output .= '</tr>';
+             foreach ($var as $col => $val) {
+                 $output .= '<td>' . $val . '</td>';
+             }
+             $output .= '</tr>';
+         } else {
+             $output .= '<tr>';
+             foreach ($var as $col => $val) {
+                 $output .= '<td>' . $val . '</td>';
+             }
+             $output .= '</tr>';
+         }
      }
-
-     public function current()
-     {
-         return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-     }
-
-     public function beginChildren()
-     {
-         echo "<tr>";
-     }
-
-     public function endChildren()
-     {
-         echo "</tr>" . "\n";
-     }
+     $output .= '</table>';
+     echo $output;
  }
 
  $servername = "localhost";
@@ -75,17 +77,11 @@
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      $stmt = $conn->prepare("SELECT * FROM $table");
      $stmt->execute();
-
-     // set the resulting array to associative
-     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-     foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-         echo $v;
-     }
+     display_data($stmt);
  } catch (PDOException $e) {
      echo "Error: " . $e->getMessage();
  }
  $conn = null;
- echo "</table>";
  ?>
 
 </body>

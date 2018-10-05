28a59aca0f3f377d686e4fe8d9b6516e4620ae69
Lucas <reymond.lu@gmail.com>
2018-10-05 13:37:43 +0200

    Ajour des parents et des jours

--- model/data/create.sql
+++ model/data/create.sql
@@ -1,3 +1,6 @@
+CREATE TABLE Jour(
+  jour text PRIMARY KEY CHECK(jour IN ('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'))
+);
 CREATE TABLE Enfant(
   id_enfant INTEGER PRIMARY KEY,
   nom varchar(50) NOT NULL,
@@ -9,19 +12,28 @@ CREATE TABLE Tuteur(
   prenom varchar(50) NOT NULL,
   pourcentage_disponibilite numeric(2,2) NOT NULL
 );
+CREATE TABLE Parent(
+  id_tuteur INTEGER,
+  id_enfant INTEGER,
+  PRIMARY KEY (id_enfant, id_tuteur),
+  FOREIGN KEY(id_tuteur) REFERENCES Tuteur(id_tuteur),
+  FOREIGN KEY(id_enfant) REFERENCES Enfant(id_enfant)
+);
 CREATE TABLE Cours(
   id_cours INTEGER PRIMARY KEY,
   libelle char(50) NOT NULL,
   heure_debut text NOT NULL,
   duree text NOT NULL,
-  jour text CHECK(jour IN ('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'))
+  jour text NOT NULL,
+  FOREIGN KEY(jour) REFERENCES Jour(jour)
 );
+
 CREATE TABLE Disponible(
   tuteur INTEGER NOT NULL,
-  cours INTEGER NOT NULL,
-  PRIMARY KEY (tuteur, cours),
+  jour text NOT NULL,
+  PRIMARY KEY (tuteur, jour),
   FOREIGN KEY(tuteur) REFERENCES Tuteur(id_tuteur),
-  FOREIGN KEY(cours) REFERENCES Cours(id_cours)
+  FOREIGN KEY(jour) REFERENCES Jour(jour)
 );
 CREATE TABLE Inscription(
   date_inscription text NOT NULL CHECK(date_inscription > date('now')),
--- model/data/insert.sql
+++ model/data/insert.sql
@@ -1,15 +1,28 @@
+--Insertion des jours
+INSERT INTO Jour VALUES ('Lundi');
+INSERT INTO Jour VALUES ('Mardi');
+INSERT INTO Jour VALUES ('Mercredi');
+INSERT INTO Jour VALUES ('Jeudi');
+INSERT INTO Jour VALUES ('Vendredi');
+INSERT INTO Jour VALUES ('Samedi');
+INSERT INTO Jour VALUES ('Dimanche');
+
 INSERT INTO Enfant(nom,prenom) VALUES('Lucas', 'REYMOND');
 INSERT INTO Enfant(nom,prenom) VALUES('Daniel', 'VALENTIN');
 
 INSERT INTO Tuteur(nom,prenom,pourcentage_disponibilite) VALUES('Papa_Lucas', 'REYMOND',3.3);
 INSERT INTO Tuteur(nom,prenom,pourcentage_disponibilite) VALUES('Papa_Daniel', 'VALENTIN', 50);
 
+-- Insertion des parents
+INSERT INTO Parent VALUES (1,1);
+INSERT INTO Parent VALUES (2,2);
+
 INSERT INTO Cours(libelle, heure_debut,duree, jour) VALUES('Piano', '17:00', '00:30','Mardi');
 INSERT INTO Cours(libelle, heure_debut,duree, jour) VALUES('Guitare', '17:00', '01:00','Mercredi');
 
 
-INSERT INTO Disponible VALUES('1','1');
-INSERT INTO Disponible VALUES('2','2');
+INSERT INTO Disponible VALUES('1','Lundi');
+INSERT INTO Disponible VALUES('2','Mardi');
 
 
 INSERT INTO Inscription(date_inscription, enfant, cours) VALUES('2018-10-29', '1', '1');

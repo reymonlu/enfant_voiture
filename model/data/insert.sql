7c3cbde920dfd4b06ca941953aed044b7cbe3abd
Loic BONNARD <bonnard.loic.07@gmail.com>
2018-10-05 13:29:50 +0200

    Beautified

--- model/data/insert.sql
+++ model/data/insert.sql
@@ -1,16 +1,56 @@
-INSERT INTO Enfant(nom,prenom) VALUES('Lucas', 'REYMOND');
-INSERT INTO Enfant(nom,prenom) VALUES('Daniel', 'VALENTIN');
-
-INSERT INTO Tuteur(nom,prenom,pourcentage_disponibilite) VALUES('Papa_Lucas', 'REYMOND',3.3);
-INSERT INTO Tuteur(nom,prenom,pourcentage_disponibilite) VALUES('Papa_Daniel', 'VALENTIN', 50);
-
-INSERT INTO Cours(libelle, heure_debut,duree, jour) VALUES('Piano', '17:00', '00:30','Mardi');
-INSERT INTO Cours(libelle, heure_debut,duree, jour) VALUES('Guitare', '17:00', '01:00','Mercredi');
+INSERT INTO Enfant(nom,prenom)
+VALUES('Lucas',
+       'REYMOND');
 
 
-INSERT INTO Disponible VALUES('1','1');
-INSERT INTO Disponible VALUES('2','2');
+INSERT INTO Enfant(nom,prenom)
+VALUES('Daniel',
+       'VALENTIN');
 
 
-INSERT INTO Inscription(date_inscription, enfant, cours) VALUES('2018-10-29', '1', '1');
-INSERT INTO Inscription(date_inscription, enfant, cours) VALUES('2018-10-30', '2', '2');
+INSERT INTO Tuteur(nom,prenom,pourcentage_disponibilite)
+VALUES('Papa_Lucas',
+       'REYMOND',
+       3.3);
+
+
+INSERT INTO Tuteur(nom,prenom,pourcentage_disponibilite)
+VALUES('Papa_Daniel',
+       'VALENTIN',
+       50);
+
+
+INSERT INTO Cours(libelle, heure_debut,duree, jour)
+VALUES('Piano',
+       '17:00',
+       '00:30',
+       'Mardi');
+
+
+INSERT INTO Cours(libelle, heure_debut,duree, jour)
+VALUES('Guitare',
+       '17:00',
+       '01:00',
+       'Mercredi');
+
+
+INSERT INTO Disponible
+VALUES('1',
+       '1');
+
+
+INSERT INTO Disponible
+VALUES('2',
+       '2');
+
+
+INSERT INTO Inscription(date_inscription, enfant, cours)
+VALUES('2018-10-29',
+       '1',
+       '1');
+
+
+INSERT INTO Inscription(date_inscription, enfant, cours)
+VALUES('2018-10-30',
+       '2',
+       '2');

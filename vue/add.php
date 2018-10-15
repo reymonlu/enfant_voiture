c07fd24f49dca416d04ee49147bac3f7f35ad4de
Loic BONNARD <bonnard.loic.07@gmail.com>
2018-10-15 13:06:35 +0200

    15 octobre update

--- vue/add.php
+++ vue/add.php
@@ -1,1 +1,16 @@
-doctype
+<!DOCTYPE html>
+<html>
+		<head>
+				<title>Demo</title>
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+				<meta name="description" content="Demo project">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.min.js"></script>
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+				<link rel="stylesheet" href="style.css">
+		</head>
+		<body ng-app>
+				<p>Let the game begin!</p>
+		</body>
+</html>

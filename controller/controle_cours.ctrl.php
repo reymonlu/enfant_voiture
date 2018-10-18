a7f6fe92577ef0ad33a22eecee930d28a2a8140b
Lucas <reymond.lu@gmail.com>
2018-10-18 10:08:38 +0200

    Ajout de quelques sécurité

--- controller/controle_cours.ctrl.php
+++ controller/controle_cours.ctrl.php
@@ -10,21 +10,24 @@
   $view->jours = $joursDAO->getAllJours();
 
 
-  # S'il manque un élément, on retourne la page avec les problemes
-  if(!isset($libelle) || !isset($heure_deb) || !isset($duree) || !isset($jour_sem)){
-    $view->error_libelle = isset($libelle) ? null : "Veuillez saisir le libelle";
-
-
-
-
-
-
+  # On test que tous les paramètres soient passés
+  if( !isset($_GET['libelle']) || !isset($_GET['heure_deb']) || !isset($_GET['duree']) || !isset($_GET['jour_sem'])){
+    $view->error_libelle = isset($_GET['libelle']) ? null : "Veuillez saisir le libelle";
+    $view->error_heure_deb = isset($_GET['heure_deb']) ? null : "Veuillez l'heure du début";
+    $view->error_duree = isset($_GET['duree']) ? null : "Veuillez la durée";
+    $view->error_jour = isset($_GET['jour_sem']) ? null : "Veuillez saisir un jour correct";
     $view->show("../vue/ajout_cours.view.php");
+    var_dump(preg_match(,"16:15"));
+    }
+
+
+  # On teste à présent la bonne intégrité des données
+  elseif ($_GET['libelle'] == null || preg_match("/^[0-9]{2}:[0-9]{2}$/",$_GET['heure_deb']) ) {
+
   }
   # Insertion dans la base de données
   else{
     $libelle = htmlentities($_GET['libelle']);
-    var_dump($libelle);
     $heure_deb = htmlentities($_GET['heure_deb']);
     $duree = htmlentities($_GET['duree']);
     $jour_sem = htmlentities($_GET['jour_sem']);
--- vue/ajout_cours.view.php
+++ vue/ajout_cours.view.php
@@ -8,17 +8,36 @@
     <h1>Rajouter un cours</h1>
     <form action="../controller/controle_cours.ctrl.php" method="get">
       <label for="libelle">Libellé : </label>
-      <input id="libelle" type="text" name="libelle" placeholder="<?= isset($this->error_libelle) ? $this->error_libelle : "" ?>"/><br>
+      <input id="libelle" type="text" name="libelle"/>
+      <?php if(isset($this->error_libelle)): ?>
+        <strong><?= $this->error_libelle ?></strong>
+      <?php endif; ?>
+      <br>
       <label for="debut">Heure de début : </label>
-      <input id="debut" type="time" name="heure_deb" placeholder=""><br>
+      <input id="debut" type="time" name="heure_deb">
+      <?php if(isset($this->error_heure_deb)): ?>
+        <strong><?= $this->error_heure_deb ?></strong>
+      <?php endif; ?>
+
+      <br>
       <label for="duree">Durée : </label>
-      <input id="duree" type="number" name="duree" placeholder=""><br>
+      <input id="duree" type="number" name="duree">
+      <?php if(isset($this->error_duree)): ?>
+        <strong><?= $this->error_duree ?></strong>
+      <?php endif; ?>
+      <br>
       <label for="jour">Jour de la semaine :</label>
         <select id="jour" name="jour_sem">
         <?php foreach ($this->jours as $jours) { ?>
           <option value="<?= $jours->getJour() ?>"><?= $jours->getJour() ?></option>
         <?php }; ?>
-        </select>
+      </select>
+      <?php if(isset($this->error_jour)): ?>
+        <strong><?= $this->error_jour ?></strong>
+      <?php endif; ?>
+
+
+      <br>
       <input type="submit" name="valider" value="OK">
     </form>
   </body>
